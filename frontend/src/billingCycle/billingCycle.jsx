a32b69133b972059ca4b6bde7c0d339516cdb616
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-07 11:21:21 -0300

    Componente TabHeader

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
-import ContentHeader from '../common/template/contentHeader'
-import Content from '../common/template/content'
-import Tabs from '../common/tab/tabs'
-import TabsHeader from '../common/tab/tabsHeader'
-import TabsContent from '../common/tab/tabsContent'
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import Tabs from '../common/tab/tabs';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import TabHeader from '../common/tab/tabHeader';
+
 
 
 class BillingCycle extends Component {
@@ -14,11 +16,14 @@ class BillingCycle extends Component {
                  <ContentHeader title='Ciclos de pagamento' small='Cadastro'/>
                  <Content>
                      <Tabs>
-                         <TabsHeader>
-                             <TabsContent>
-
-                             </TabsContent>
-                         </TabsHeader>
+                        <TabsHeader>
+                             <TabHeader label='Listar' icon='bars' target='tabList'/>
+                             <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+                             <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+                        </TabsHeader>
+                        <TabsContent>
+                        </TabsContent>
                      </Tabs>
                  </Content>
             </div>
