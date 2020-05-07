e7a4a95d7ffff2308b117b8ff3d4b61973d71724
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-07 19:13:49 -0300

    exibindo mensagens de sucesso e erro ao incluir pagamentos

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,14 +5,16 @@ import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
 import Footer from '../common/template/footer';
 import Routes from './routes';
+import Messages from '../common/msg/messages';
 
 export default props => (
     <div className='wrapper'>
         <Header/>
         <Sidebar/>
         <div className='content-wrapper'>
-            <Routes/>
+            <Routes />
         </div>
-        <Footer/>
+        <Footer />
+        <Messages />
     </div>
 );
