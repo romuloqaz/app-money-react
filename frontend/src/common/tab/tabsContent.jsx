be8025ffc4fae3b718be6b8910c41f83bf824a07
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-07 11:00:20 -0300

    componentes estruturais das abas

---
+++ frontend/src/common/tab/tabsContent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div className='tab-content'>
+        {props.children}
+    </div>
+);
+// agrupar todos os componentes das abas
---
+++ frontend/src/common/tab/tabsHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props =>(
+    <ul className='nav nav-tabs'>
+        {props.children}
+    </ul>
+);
+
+//agrupar todos os cabeçalhos das abas
