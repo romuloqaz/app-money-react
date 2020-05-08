093e7aa96c7b30453378eee666e699ca2860800a
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-08 12:11:45 -0300

    função do botão cancelar e zerar inicialização dos formularios de incluir

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -5,6 +5,7 @@ import { showTabs, selectTab } from '../common/tab/tabActions';
  
 
 const BASE_URL = 'http://localhost:3003/api';
+const INITIAL_VALUES = {};
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`);
@@ -19,13 +20,7 @@ export function create(values) {
         axios.post(`${BASE_URL}/billingCycles`, values)
             .then(resp => {
                 toastr.success('Sucesso', 'Operação realizada com sucesso.');
-                dispatch([
-                    // so é possivel array com redux multi
-                    resetForm('billingCycleForm'), 
-                    getList(),
-                    selectTab('tabList'),
-                    showTabs('tabList','tabCreate')
-                ])
+                dispatch(init());
             })
             .catch(e => {
                 e.response.data.errors.forEach(error => toastr.error('Erro', error))
@@ -36,8 +31,18 @@ export function create(values) {
 
 export function showUpdate(billingCycle){
     return [
+        //redux multi
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingCycle)
-    ]
+    ];
+};
+
+export function init (){
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
+    ];
 };
