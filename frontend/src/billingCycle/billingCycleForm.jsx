f2b9f3d51404dd19965cfb67a624266005f3509e
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-08 16:24:29 -0300

    correção css: adicionando nome distanciamento entre botoes

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {reduxForm, Field} from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 import { init } from './billingCycleActions';
 import LabelAndInput from '../common/form/labelAndInput';
 
 class BillingCycleForm extends Component {
-    render(){
-        const {handleSubmit, readOnly} = this.props;
-        return(
+    render() {
+        const { handleSubmit, readOnly } = this.props;
+        return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                <Field name='name' component={LabelAndInput} readOnly={readOnly}
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Mês' cols='12 4' placeholder='Informe o mês' />
@@ -20,7 +20,9 @@ class BillingCycleForm extends Component {
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
                 </div>
                 <div className='box-footer'>
-                    <button type='submit' className='btn btn-primary'>Submit</button>
+                    <button type='submit' className={`btn btn-${this.props.submitClass}`}>
+                        {this.props.submitLabel}
+                    </button>
                     <button type='button' className='btn btn-default'
                         onClick={this.props.init}>Cancelar</button>
                 </div>
@@ -29,6 +31,6 @@ class BillingCycleForm extends Component {
     };
 };
 
-BillingCycleForm = reduxForm ({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm);
-const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch);
+BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm);
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycleForm);
