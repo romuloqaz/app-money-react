d518fb998da7248c34d50128fa78bf8456b3d0e8
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-08 10:59:37 -0300

    melhorias na inclusão do pagamento: redirecionando paginas, limpando campos e mostrando abas

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
-import {toastr} from 'react-redux-toastr';
+import { toastr } from 'react-redux-toastr';
+import {reset as resetForm} from 'redux-form';
+import { showTabs, selectTab } from '../common/tab/tabActions';
+ 
 
 const BASE_URL = 'http://localhost:3003/api';
 
-export function getList(){
+export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`);
     return {
         type: 'BILLING_CYCLES_FETCHED',
@@ -11,15 +14,22 @@ export function getList(){
     };
 };
 
-export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`,values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação realizada com sucesso.')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        });
-    return {
-        type: 'TEMP'
+export function create(values) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso.');
+                dispatch([
+                    // so é possivel array com redux multi
+                    resetForm('billingCycleForm'), 
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList','tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            });
     }
+
 };
