fb66e06bb6ad0117cecb7c7d06a34a376f9839b6
romuloqaz <romulo.pereiracz@gmail.com>
2020-05-08 18:39:52 -0300

    adicionar e clonar creditos

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,63 +1,60 @@
-import axios from 'axios';
-import { toastr } from 'react-redux-toastr';
-import { reset as resetForm, initialize } from 'redux-form';
-import { showTabs, selectTab } from '../common/tab/tabActions';
+import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm, initialize } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
-
-const BASE_URL = 'http://localhost:3003/api';
-const INITIAL_VALUES = {creditis: [{}]};
+const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {credits: [{}]}
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`);
+    const request = axios.get(`${BASE_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
-    };
-};
+    }
+}
 
 export function create(values) {
-    return submit(values, 'post');
-};
+    return submit(values, 'post')
+}
 
 export function update(values) {
-    return submit(values, 'put');
+    return submit(values, 'put')
 }
 
 export function remove(values) {
-    return submit(values, 'delete');
+    return submit(values, 'delete')
 }
 
 function submit(values, method) {
     return dispatch => {
-        const id = values._id ? values._id : '';
+        const id = values._id ? values._id : ''
         axios[method](`${BASE_URL}/billingCycles/${id}`, values)
             .then(resp => {
-                toastr.success('Sucesso', 'Operação realizada com sucesso.');
-                dispatch(init());
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch(init())
             })
             .catch(e => {
                 e.response.data.errors.forEach(error => toastr.error('Erro', error))
-            });
+            })
     }
 }
 
 export function showUpdate(billingCycle) {
-    return [
-        //redux multi
+    return [ 
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingCycle)
-    ];
-};
+    ]
+}
 
 export function showDelete(billingCycle) {
-    return [
-        //redux multi
+    return [ 
         showTabs('tabDelete'),
         selectTab('tabDelete'),
         initialize('billingCycleForm', billingCycle)
-    ];
-};
+    ]
+}
 
 export function init() {
     return [
@@ -65,5 +62,5 @@ export function init() {
         selectTab('tabList'),
         getList(),
         initialize('billingCycleForm', INITIAL_VALUES)
-    ];
-};
+    ]
+}
